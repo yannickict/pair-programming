9c96a3bce6949b7402e94dcddc6b2fd870f7e8e2
yannickict <yblatty@edp.ch>
2025-04-04 10:36:48 +0200

    Aufgabe 01 TDD Pair Programming

--- src/vacation-days.test.ts
+++ src/vacation-days.test.ts
@@ -16,3 +16,63 @@ test("full year 100% employment gives all vacation days", () => {
   // Assert
   expect(actual).toBe(expected);
 });
+
+
+
+
+test("full year 60% employment gives reduced vacation days", () => {
+ // Arrange
+ const fullTime: Employment = {
+  startDate: new Date(Date.parse("2025-01-01 00:00:00+01:00")),
+  untilDate: new Date(Date.parse("2025-12-31 23:59:59+01:00")),
+  percentage: 60,
+  vacationDays: 25,
+};
+const expected = 15;
+
+// Act
+const actual = calculateProRataVacationDays(fullTime);
+
+// Assert
+expect(actual).toBe(expected);
+
+
+});
+
+test("partial year 100% employment (half year)", () => {
+  // Arrange
+  const time: Employment = {
+    startDate: new Date("2025-01-01 00:00:00+01:00"),
+    untilDate: new Date("2025-06-30 23:59:59+01:00"),
+    percentage: 100,
+    vacationDays: 25,
+  };
+
+
+  const expected = 12;
+
+  // Act
+  const actual = calculateProRataVacationDays(time);
+
+  // Assert
+  expect(actual).toBe(expected);
+});
+
+test("partial year 70% employment (Jan - Mar)", () => {
+  // Arrange
+  const time: Employment = {
+    startDate: new Date("2025-01-01 00:00:00+01:00"),
+    untilDate: new Date("2025-03-31 23:59:59+01:00"),
+    percentage: 70,
+    vacationDays: 25,
+  };
+
+
+  const expected = 4;
+
+  // Act
+  const actual = calculateProRataVacationDays(time);
+
+  // Assert
+  expect(actual).toBe(expected);
+});
--- src/vacation-days.ts
+++ src/vacation-days.ts
@@ -1,3 +1,5 @@
+import { isLeapYear } from "./utils";
+
 export type Employment = {
   startDate: Date;
   untilDate: Date;
@@ -5,7 +7,23 @@ export type Employment = {
   vacationDays: number;
 };
 
+
+
+
 export function calculateProRataVacationDays(employment: Employment): number {
-  // TODO: calculate pro rata (consider workload and days worked)
-  return employment.vacationDays;
+  const { startDate, untilDate, percentage, vacationDays } = employment;
+  
+  let totalYearDays = 365
+  if (isLeapYear(startDate.getFullYear())){
+    totalYearDays = 366
+  }
+
+ 
+
+  const timeDiff = untilDate.getTime() - startDate.getTime();
+   const actualDays = (timeDiff / (1000 * 60 * 60 * 24)) + 1;
+
+
+     return Math.round(vacationDays * (actualDays / totalYearDays) * (percentage / 100) ) ;
+
 }
