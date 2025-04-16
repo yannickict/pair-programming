2f14ce55e08486586d1fbb70cabc7f74659d5c79
MARVIN HEGI <MARVIN_HEGI@sluz.ch>
2025-04-16 20:45:55 +0200

    corectet de values for the tests and implementet test for 21 year old

--- src/payroll.test.ts
+++ src/payroll.test.ts
@@ -15,9 +15,13 @@ describe("Calculate Payslip", () => {
 
         const result: Payslip = {
             salary,
-            deductions: new Map(),
-            totalDeductions: 0.0,
-            net: 700,
+            deductions: new Map([
+                ["ALV", 7.7],
+                ["NBU", 5.11],
+
+            ]),
+            totalDeductions: 12.81,
+            net: 687.19,
         };
 
 
@@ -30,6 +34,84 @@ describe("Calculate Payslip", () => {
         expect(payslip).toEqual(result);
 
     });
+
+
+
+
+    test("Payslip 18 year old", () => {
+
+        //Arrange
+
+        const salary: Salary = {
+            born: new Date("2000-01-01"),
+            payday: new Date("2018-01-02"),
+            gross: 1200,
+        };
+
+
+        const result: Payslip = {
+            salary,
+            deductions: new Map([
+                ["AHV", 104.4],
+                ["IV", 16.8],
+                ["EO", 6],
+                ["ALV", 13.2],
+                ["NBU", 8.76],
+                
+            ]),
+            totalDeductions: 149.61,
+            net: 1050.84,
+        };
+
+
+        //Act
+
+        const payslip = calculatePayslip(salary);
+
+        //Assert
+
+        expect(payslip).toEqual(result);
+
+    });
+
+
+    test("Payslip 21 year old", () => {
+
+        //Arrange
+
+        const salary: Salary = {
+            born: new Date("2000-01-01"),
+            payday: new Date("2021-01-02"),
+            gross: 5900,
+        };
+
+
+        const result: Payslip = {
+            salary,
+            deductions: new Map([
+                ["AHV", 513.3],
+                ["IV", 82.6],
+                ["EO", 29.5],
+                ["ALV", 64.9],
+                ["NBU", 43.07],
+                ["PK", 525.1],
+            ]),
+            totalDeductions: 1258.47,
+            net: 4641.53,
+        };
+
+
+        //Act
+
+        const payslip = calculatePayslip(salary);
+
+        //Assert
+
+        expect(payslip).toEqual(result);
+
+    });
+
+
 
 
 
